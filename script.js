9652ca1e52774158574d097965b85de9b841d0e4
Lvga21 <132945697+lVega21@users.noreply.github.com>
2024-02-23 13:45:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let num1=10,num2=2,num3=5;
+
+if(num1>num2){
+    if (num2>num3){
+        console.log(num1);
+    }
+
+}
+
+if(num2>num1){
+    if (num2>num3){
+        console.log(num2);
+    }
+
+}
+
+if(num3>num1){
+    if (num3>num2){
+        console.log(num3);
+    }
+
+}
