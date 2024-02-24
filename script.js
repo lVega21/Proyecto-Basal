e642b0c231f8d007d9de5ba56ce02e8f1d49e6e2
Lvga21 <132945697+lVega21@users.noreply.github.com>
2024-02-24 17:54:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-let num1=10,num2=2,num3=5;
+function calculos(){
+        var inputPeso = (document.getElementById('peso').value);
+        var peso= parseFloat (inputPeso);
+        var valorDiario;
+        var matD_HS, matM_HS; 
+        var matD_SC, matM_SC, matD2_SC, matM2_SC;
+        var mensajeError = document.getElementById('error');
 
-if(num1>num2){
-    if (num2>num3){
-        console.log(num1);
-    }
-
+if ( isNaN(peso) || peso<1){         //está verificando si "peso" no es un número o si inputPeso está vacío
+    mensajeError.textContent = "Valor no valido, intentelo de nuevo";
+return;
+}else{
+    mensajeError.textContent = "";
 }
 
-if(num2>num1){
-    if (num2>num3){
-        console.log(num2);
-    }
+// peso en H-Segar
+if (peso<=10){
+    valorDiario = (peso *100);
+}
+else if (peso <=20){
+    valorDiario = ( ( (peso-10) *50) + 1000);
+}
+else {
+    valorDiario = ( ( (peso-20) *20) + 1500);
+}
+//matenimiento H-Segar
+matD_HS= Math.ceil(valorDiario / 24);
+matM_HS= Math.ceil(matD_HS + ( matD_HS/ 2) );
+ 
 
+
+//peso en SC
+if (peso>30){
+matD_SC=Math.ceil(( ( ( ( peso*4) +7) *1500) / (peso+90) )/24);
+matM_Sc=Math.ceil(matD_SC + (matD_SC / 2) )
+
+matD2_SC=Math.ceil( ( ( ( ( peso*4) +7) *2000) / (peso+90) )/24);
+matM2_SC=Math.ceil(matD2_SC + (matD2_SC / 2) );        
+
+document.getElementById('mante').textContent = 'x1500:\t' + matD_SC + '\tcc/hr\t' + matM_Sc + 'cc/hr\nx2000:\t' + 
+                                                matD2_SC + '\tcc/hr\t'+matM2_SC+'\tcc/hr';}
+
+else {
+  
+  document.getElementById('mante').textContent = matD_HS + "\tcc/hr\nm+m/2:\t" + matM_HS + "\tcc/hr\t";}
 }
 
-if(num3>num1){
-    if (num3>num2){
-        console.log(num3);
-    }
 
-}
+
+
+
